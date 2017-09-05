b9ca0e1e67043c40af388ee55d583f22b3e9975f
cmdrafa <rkraemer01@gmail.com>
2017-09-05 17:54:32 +0100

    Finished auth flow with env variables

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod keys
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: 'mongodb://localhost/emaily-prod',
+    cookieKey: process.env.COOKIE_KEY
+};
